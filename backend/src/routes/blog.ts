600c95a46e95c42af4875edec0ca9ae2065146ed
aditya <adityaedu22@gmail.com>
2024-05-26 10:11:34 +0530

    Deploy on cloudflare(backend)

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -15,13 +15,19 @@ export const blogRouter = new Hono<{
 
 blogRouter.use('/*', async (c, next) => {
 	const token = c.req.header("authorization") || "";
-	const user = await verify(token, c.env.JWT_SECRET)
-
+    try {
+        const user = await verify(token, c.env.JWT_SECRET)
 	if(user) {
-        
+        //@ts-ignore
         c.set("userId", user.id);
         await next();
     } else {
+        c.status(403); 
+        return c.json({
+            message: "You are unable to log in"
+        })
+    }
+    } catch(e) {
         c.status(403); 
         return c.json({
             message: "You are unable to log in"
